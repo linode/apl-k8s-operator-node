f3f0b26861b2f98dd8617583d1417fd247904d62
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2025-01-16 10:09:39 +0100

    feat: build package (#5)
    * chore(release): 0.1.0
    
    * chore(release): 0.1.0
    
    * feat: added build package and publish
    
    * fix: package name check
    
    * feat: requested changes
    
    * fix: only publish on main
    
    * fix: only publish on main and chore(releases)
    
    ---------
    
    Co-authored-by: Cas Lubbers <clubbers@akamai.com>

---
+++ bin/publish-package.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+version=$(jq -r '.version' package.json)
+
+unpublished_exists() {
+  local version=$1
+  if [ -n "$(npm info "@linode/apl-k8s-operator@$version")" ]; then
+    return 0
+  fi
+  return 1
+}
+
+release() {
+  if ! unpublished_exists $version; then
+    local pkg="@linode/apl-k8s-operator@$version"
+    echo "Publishing newer package: $pkg"
+    npm publish --access public
+    cd -
+  fi
+}
+
+release
